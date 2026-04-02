8c7032bbf01e78a80d3a8b59bc1f2058ce634e5c
Ruixin Wu <781785786@qq.com>
2026-04-02 14:34:35 +0800

    [add] add manipulate_pipette envs and pipette asset

---
+++ launch/manipulate_pipette.sh
@@ -0,0 +1,4 @@
+python -m scripts.run_env --gym_config configs/manipulate_pipette/gym_config.json \
+       --action_config configs/manipulate_pipette/action_config.json \
+       --filter_visual_rand \
+       --num_envs 1 \
