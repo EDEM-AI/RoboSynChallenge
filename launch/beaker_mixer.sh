1016775578b36a8d676d108c62085edd20ecd9ca
Ruixin Wu <781785786@qq.com>
2026-04-14 12:26:56 +0800

    [modify] modify transparent material

--- launch/beaker_mixer.sh
+++ launch/beaker_mixer.sh
@@ -1,5 +1,6 @@
 python -m scripts.run_env --gym_config configs/beaker_mixer/gym_config.json \
        --action_config configs/beaker_mixer/action_config.json \
-       --filter_visual_rand \
        --num_envs 1 \
-       # --enable_rt \
+       --enable_rt \
+       # --filter_visual_rand \
+
--- launch/sample_loading.sh
+++ launch/sample_loading.sh
@@ -1,4 +1,5 @@
 python -m scripts.run_env --gym_config configs/sample_loading/gym_config.json \
        --action_config configs/sample_loading/action_config.json \
-       --filter_visual_rand \
        --num_envs 1 \
+       --enable_rt \
+       # --filter_visual_rand \
