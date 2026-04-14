91d8e725d502fab907f6f419f822fc5abbfbf716
Ruixin Wu <781785786@qq.com>
2026-04-14 14:26:31 +0800

    [add] add beaker_mixer duel arm env

---
+++ launch/beaker_mixer_duel.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python -m scripts.run_env \
+    --gym_config configs/beaker_mixer/gym_config_duel.json \
+    --action_config configs/beaker_mixer/action_config_duel.json \
+    --num_envs 1 \
+    --enable_rt
