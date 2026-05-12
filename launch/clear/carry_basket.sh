e81f39021cc317aedec811e191e463a80f79457b
Chengkun Li <213220813@seu.edu.cn>
2026-05-12 14:50:05 +0800

    rearr_carry

--- launch/clear/carry_basket.sh
+++ launch/clear/carry_basket.sh
@@ -1,6 +1,7 @@
-python -m scripts.run_env --gym_config configs/carry_basket/gym_config_random.json \
+python -m scripts.run_env --gym_config configs/carry_basket/gym_config_clear.json \
        --action_config configs/carry_basket/action_config.json \
        --num_envs 1 \
-       --enable_rt \
+      # --enable_rt \
       # --filter_visual_rand \
+      # --preview
       # --enable_rt \
